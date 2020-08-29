3dc05ba1de6d572edc905f86c7b2ed6f7a8e2277
Mahmoud Kassify <mahmoud.k.elkassify@gmail.com>
2020-08-29 20:05:40 +0200

    add new files

--- Platforms/tm4c123gh6pm/Platform_Defines.h
+++ Platforms/tm4c123gh6pm/Platform_Defines.h
@@ -1,8 +1,8 @@
 /****************************************************************************/
 /** Copyright (c) 2020 Mahmoud Kassify. All Rights Reserved. 			   **/
 /****************************************************************************/
-#ifndef _H_PLATFORM_
-#define _H_PLATFORM_
+#ifndef _H_PLATFORM_DEFINES_
+#define _H_PLATFORM_DEFINES_
 
 #define PERIPHERAL_BITBAND_REGION           (0x20000000)
 #define PERIPHERAL_BITBAND_ALIAS            (0x22000000)
@@ -10,4 +10,4 @@
 #define RAM_BITBAND_REGION                  (0x40000000)
 #define RAM_BITBAND_ALIAS                   (0x42000000)
 
-#endif /*_H_PLATFORM_*/
+#endif /*_H_PLATFORM_DEFINES_*/
--- Src/Common/Platform_Types.h
+++ Src/Common/Platform_Types.h
@@ -8,6 +8,26 @@
 /*- INCLUDES ----------------------------------------------*/
 /*---------------------------------------------------------*/
 #include <stdint.h>
+#include <stdbool.h>
+
+
+#define TRUE    (bool)true
+#define FALSE   (bool)false
+
+
+typedef float				float32;
+typedef double				float64;
+
+typedef bool				boolean;
+
+typedef int8_t				sint8;  
+typedef uint8_t				uint8;  
+typedef int16_t				sint16; 
+typedef uint16_t			uint16; 
+typedef int32_t				sint32; 
+typedef uint32_t			uint32; 
+typedef int64_t				sint64;
+typedef uint64_t			uint64; 
 
 
 #endif /*_H_TYPES_*/
